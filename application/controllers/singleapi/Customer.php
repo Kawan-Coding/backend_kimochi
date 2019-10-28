ff3c8049ff6eadde874dbef08848570c393899d5
rudiwawa <rudiwawa1980@gmail.com>
2019-10-28 12:02:33 +0700

    0.1.20.1 perbaikan algoritma customer

--- application/controllers/singleapi/Customer.php
+++ application/controllers/singleapi/Customer.php
@@ -30,7 +30,8 @@ class Customer extends CI_Controller
     function is_valid()
     {
         if (isset($_POST) && count($_POST) <= 0) {
-            $this->msg('', '400', '','tidak ada masukan');        }
+            $this->msg('', '400', '', 'tidak ada masukan');
+        }
     }
 
     public function get_all()
@@ -46,12 +47,25 @@ class Customer extends CI_Controller
         if ($res['status']) {
             $this->msg('data', '200', $res['data']);
         } else {
-            $this->msg('data', '400','', $res['data']['message']);
+            $this->msg('data', '400', '', $res['data']['message']);
             // $this->msg('data', '400',$res);
         };
     }
 
-
+    public function upload_image()
+    {
+        $config['upload_path'] = './uploads/customer';
+        $config['allowed_types'] = 'gif|jpg|png|JPG';
+        $config['encrypt_name'] = true;
+        // $config['max_size'] = 600;
+        // $config['max_width'] = 1024;
+        // $config['max_height'] = 768;
+        $this->load->library('upload', $config);
+        $is_upload_succces = $this->upload->do_upload('foto');
+        $file = $this->upload->data();
+        $error_upload = array('img' => $this->upload->display_errors());
+        return array('status' => $is_upload_succces, 'name' => $file['file_name'], 'error_msg' => $error_upload);
+    }
 
     /*
      * Adding a new produk
@@ -59,6 +73,7 @@ class Customer extends CI_Controller
     function add()
     {
         $this->is_valid();
+        $file_foto = $this->upload_image();
         $params = array(
             'password' => password_hash($this->input->post['password'], PASSWORD_DEFAULT),
             'username' => $this->input->post('username'),
@@ -67,17 +82,25 @@ class Customer extends CI_Controller
             'kendaraan' => $this->input->post('kendaraan'),
             'plat_nomor' => $this->input->post('plat_nomor'),
             'member' => $this->input->post('member'),
-            'create_at' => date('Y-m-d H:i:s'),
-            // 'update_at' => $this->input->post('update_at'),
-            'foto' => $this->input->post('foto'),
-        );
 
-        $res = $this->Master->add($this->tabel, $params);
-        if ($res['status']) {
-            $this->msg('data', '200', $res['data']);
+            'tanggal_lahir' => $this->input->post('tanggal_lahir'),
+            'gender' => $this->input->post('gender'),
+            'email' => $this->input->post('email'),
+
+            'create_at' => date('Y-m-d H:i:s'),
+            'update_at' => date('Y-m-d H:i:s'),
+            'foto' => $file_foto['name'],
+        );
+        if ($file_foto['status']) {
+            $res = $this->Master->add($this->tabel, $params);
+            if ($res['status']) {
+                $this->msg('data', '200', $res['data']);
+            } else {
+                $this->msg('data', '400', '', $res['data']['message']);
+            };
         } else {
-            $this->msg('data', '400','', $res['data']['message']);
-        };
+            $this->msg('data', '400', '', $file_foto['error_msg']['img']);
+        }
     }
 
     /*
@@ -89,23 +112,53 @@ class Customer extends CI_Controller
         // check if the produk exists before trying to edit it
         $id =  $this->input->post('id');
         $data = array(
-            'password' => password_hash($_POST['password'], PASSWORD_DEFAULT),
             'username' => $this->input->post('username'),
             'nama_lengkap' => $this->input->post('nama_lengkap'),
             'no_telepon' => $this->input->post('no_telepon'),
             'kendaraan' => $this->input->post('kendaraan'),
             'plat_nomor' => $this->input->post('plat_nomor'),
             'member' => $this->input->post('member'),
+
+            'tanggal_lahir' => $this->input->post('tanggal_lahir'),
+            'gender' => $this->input->post('gender'),
+            'email' => $this->input->post('email'),
+
             'update_at' => date('Y-m-d H:i:s'),
             // 'create_at' => date('Y-m-d H:i:s'),
-            'foto' => $this->input->post('foto'),
         );
-        $res = $this->Master->update($this->tabel,  array('id' => $id), $data);
-        if ($res['status']) {
-            $this->msg('data', '200', $res['data']);
+        if (!empty($_POST['password'])) {
+            $data['password'] = password_hash($_POST['password'], PASSWORD_DEFAULT);
+        }
+        if (!empty($_FILES['foto']['name'])) {
+            $file_foto = $this->upload_image();
+            $data['foto'] = $file_foto['name'];
+            if ($file_foto['status']) {
+                $res = $this->Master->update($this->tabel,  array('id' => $id), $data);
+                if ($res['status']) {
+                    $this->msg('data', '200', $res['data']);
+                } else {
+                    $this->msg('data', '400', '', $res['data']['message']);
+                };
+            } else {
+                $this->msg('data', '400', '', $file_foto['error_msg']['img']);
+            }
         } else {
-            $this->msg('data', '400','', $res['data']['message']);
-        };
+            // unset($params['foto']);
+            $res = $this->Master->update($this->tabel,  array('id' => $id), $data);
+            if ($res['status']) {
+                $this->msg('data', '200', $res['data']);
+            } else {
+                $this->msg('data', '400', '', $res['data']['message']);
+            };
+        }
+
+
+        // $res = $this->Master->update($this->tabel,  array('id' => $id), $data);
+        // if ($res['status']) {
+        //     $this->msg('data', '200', $res['data']);
+        // } else {
+        //     $this->msg('data', '400', '', $res['data']['message']);
+        // };
     }
 
     /*
@@ -149,7 +202,7 @@ class Customer extends CI_Controller
         if ($res['status']) {
             $this->msg('data', '200', $res['data']);
         } else {
-            $this->msg('data', '400','', $res['data']['message']);
+            $this->msg('data', '400', '', $res['data']['message']);
         };
     }
 
